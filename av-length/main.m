077f6f7c5694976978fc85ff5ab627cb2999137a
Jamie Bullock <jamie@jamiebullock.com>
2013-03-28 13:51:10 +0000

    Add -types argument to list types supported by av-length

--- av-length/main.m
+++ av-length/main.m
@@ -3,20 +3,55 @@
 //  av-length
 //
 //  Created by Jamie Bullock on 28/03/2013.
-//  Copyright (c) 2013 Jamie Bullock. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+void displayUsageStatement(NSString *appPath);
+Float64 durationInSecondsForSoundfileWithPath(NSString *soundfilePath);
 
 int main(int argc, const char * argv[])
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+    @autoreleasepool
+    {
+        if (argc != 2)
+        {
+            NSString *appPath = [NSString stringWithUTF8String:argv[0]];
+            displayUsageStatement(appPath);
+        }
+        else
+        {
+            NSString *firstArgument = [NSString stringWithUTF8String:argv[1]];
+            
+            if ([firstArgument isEqualToString:@"-types"])
+            {
+                printf("Supported MIME types:\n");
+                NSString *mimeTypesString = [NSString stringWithFormat:@"%@", [AVURLAsset audiovisualMIMETypes]];
+                printf("%s\n", [mimeTypesString UTF8String]);
+            }
+            else
+            {
+                Float64 soundfileDuration = durationInSecondsForSoundfileWithPath(firstArgument);
+                printf("%f\n", soundfileDuration);
+            }
+        }
     }
     return 0;
 }
 
+void displayUsageStatement(NSString *appPath)
+{
+    NSString *appName = [appPath lastPathComponent];
+    printf("Usage: %s [-types] <soundfile path>\n", [appName UTF8String]);
+}
+
+Float64 durationInSecondsForSoundfileWithPath(NSString *soundfilePath)
+{
+    NSURL *soundfileURL = [NSURL fileURLWithPath:soundfilePath];
+    AVURLAsset* audioAsset = [AVURLAsset URLAssetWithURL:soundfileURL options:nil];
+    CMTime audioDuration = audioAsset.duration;
+    CGFloat audioDurationSeconds = CMTimeGetSeconds(audioDuration);
+    
+    return audioDurationSeconds;
+}
