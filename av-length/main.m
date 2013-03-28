d2e4098482f4b247f5ef5880df05e704f880df6d
Jamie Bullock <jamie@jamiebullock.com>
2013-03-28 13:24:41 +0000

    Initial Commit

---
+++ av-length/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  av-length
+//
+//  Created by Jamie Bullock on 28/03/2013.
+//  Copyright (c) 2013 Jamie Bullock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
